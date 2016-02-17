146e19d12d3f4aa6fcbcd517cead8429392eda12
Mazin Power <mazin.power@icloud.com>
2016-02-17 18:30:04 +0000

    Spike for Phase I of progress tracking features

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -27,12 +27,20 @@ class LessonsController < ApplicationController
 
     # enable SSO for disqus in this lesson
     @discusjs = disqus_sso if user_signed_in?
-    
+
     @content = md(@lesson.content) if @lesson.content
     @next_lesson = @lesson.next_lesson
     @prev_lesson = @lesson.prev_lesson
     @num_lessons = @lesson.section.lessons.where(:is_project => false).count
     @num_projects = @lesson.section.lessons.where(:is_project => true).count
+
+    #SPIKE! These two queries run the logic we want - but are by no means final.
+    #TODO: Write tests, rewrite this crap :)
+    @completion_ids = @lesson.lesson_completion_ids
+    @current_students = @prev_lesson.lesson_completion_ids.find_all do |completion_id|
+      User.find(completion_id).send(:latest_completed_lesson).id == @prev_lesson.id
+    end
+
     # the position of the lesson not including projects
     @lesson_position_in_section = @lesson.section.lessons.where("is_project = ? AND position <= ?", false, @lesson.position).count
     # puts "\n\nVE BANNER #{Ad.show_ads?(current_user)}!\n\n"
